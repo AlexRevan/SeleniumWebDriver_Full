823d52fec349ee55ac99e20f14b0d6d6c0174c70
Alexey Parunov <alexey-parunov@yandex.ru>
2021-06-24 15:51:43 +0300

    Update LoginToAdminPanel.js

--- LoginToAdminPanel.js
+++ LoginToAdminPanel.js
@@ -1,6 +1,9 @@
 const { Builder, By, until } = require('selenium-webdriver')
+const chrome = require('selenium-webdriver/chrome');
+const options = new chrome.Options();
+options.addArguments(["start-maximized"]);
 
-const driver = new Builder().forBrowser('chrome').build();
+const driver = new Builder().withCapabilities(({'unexpectedAlertBehaviour': 'dismiss'})).forBrowser('chrome').setChromeOptions(options).build();
 
 driver
     .get('http://localhost/litecart/admin/')
@@ -14,6 +17,7 @@ driver
         driver.findElement(By.name('login')).click()
     )
     .then((_) => driver.wait(until.elementIsVisible(driver.findElement(By.xpath('//*[@title="My Store"]'))), 1000))
+
     .then((_) => driver.quit())
 
 
